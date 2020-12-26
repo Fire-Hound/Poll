502e43a2e67d4c65e66423853f8980dc8afc2001
Vikram Cothur <vcothur7@gmail.com>
2020-12-26 18:51:14 +0530

    token

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :nostrum,
-  token: System.get_env("discordtokenpoll"), # The token of your bot as a string
+  token: System.get_env("discordtokenpoll") |> IO.inspect(), # The token of your bot as a string
   num_shards: :auto, # The number of shards you want to run your bot under, or :auto.
   gateway_intents: [
     :guild_messages
